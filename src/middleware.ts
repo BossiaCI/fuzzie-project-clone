8c5fce4e3fdecaf7c81c27ee42882ac1151018d6
BossiaCI <bossiadaha@gmail.com>
2025-03-28 13:38:14 +0100

    web-progigies fuzzie application clone

---
+++ src/middleware.ts
@@ -0,0 +1,39 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoutes = createRouteMatcher([
+    '/',
+    '/api/clerk-webhook',
+    '/api/drive-activity/notification',
+    '/api/payment/success',
+    '/sign-in(.*)',
+    '/sign-up(.*)'
+])
+
+const isIgnoredRoutes =  createRouteMatcher([
+    '/api/auth/callback/discord',
+    '/api/auth/callback/notion',
+    '/api/auth/callback/slack',
+    '/api/flow',
+    '/api/cron/wait',
+  ])
+
+export default clerkMiddleware(async (auth, req) => {
+
+    if (!isPublicRoutes(req) && !isIgnoredRoutes(req)) await auth.protect()
+
+})
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
+
+// https://www.googleapis.com/auth/userinfo.email
+// https://www.googleapis.com/auth/userinfo.profile
+// https://www.googleapis.com/auth/drive.activity.readonly
+// https://www.googleapis.com/auth/drive.metadata
+// https://www.googleapis.com/auth/drive.readonly
